bf75463944ce871c72bf0293bf6248a491205d3d
Aron-j <xy@ichaoj.com>
2020-04-26 20:56:40 +0800

    local debug properties be added; read the check poc account from the propertied of the local debug

--- src/main/java/org/conch/util/LocalDebugTool.java
+++ src/main/java/org/conch/util/LocalDebugTool.java
@@ -5,12 +5,36 @@ import org.apache.commons.lang3.StringUtils;
 import org.conch.account.Account;
 import org.conch.env.RuntimeEnvironment;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Used to local debug, contains all debug dirty code
  */
 public class LocalDebugTool {
+
+    private static final String LOCAL_DEBUG_CONFIG = "local-debug.properties";
+    private static Properties localDebugConfig = new Properties();
+    private static final String SEPARATOR = ",";
+    static {
+        Path confDir = Paths.get(".", "conf");
+        Path propPath = Paths.get(confDir.toString()).resolve(Paths.get(LOCAL_DEBUG_CONFIG));
+        if (Files.isReadable(propPath)) {
+            System.out.printf("Loading %s from dir %s\n", localDebugConfig, confDir);
+            try {
+                localDebugConfig.load(Files.newInputStream(propPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static final String DEBUG_PROPERTY = "debug.check.poc.accounts";
+
     /**
      * used to local debug
      * @return true： local debug mode
@@ -21,9 +45,7 @@ public class LocalDebugTool {
     }
 
     // read from the debug config
-    private static final List<String> checkPocAccounts = Lists.newArrayList(
-
-    );
+    private static final List<String> checkPocAccounts = Lists.newArrayList(localDebugConfig.getProperty(DEBUG_PROPERTY,"").split(SEPARATOR));
 
     public static boolean isCheckPocAccount(String rsAccount){
        return isLocalDebug() && checkPocAccounts.contains(rsAccount);
