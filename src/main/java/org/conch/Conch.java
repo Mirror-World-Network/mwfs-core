425b7b35363a6a526f1037991a820d19bc4d4951
Aron-j <xy@ichaoj.com>
2020-04-29 00:04:05 +0800

    version upgrade to v0.0.3

--- src/main/java/org/conch/Conch.java
+++ src/main/java/org/conch/Conch.java
@@ -97,7 +97,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public final class Conch {
 
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.0.3";
     // Phase: Pioneer -> Union -> World
     public static final String STAGE = "Pioneer";
     public static final String APPLICATION = "COS";
