e8aed294e0a0cc5dfed30b78ec6193cc20045050
Aron-j <xy@ichaoj.com>
2020-06-09 00:43:46 +0800

    change the force re-calculate height to 7288

--- src/main/java/org/conch/common/Constants.java
+++ src/main/java/org/conch/common/Constants.java
@@ -230,8 +230,7 @@ public final class Constants {
     // NOTE: set the height to 0 when reset the chain or start a new chain
     public static final int POC_CAL_ALGORITHM = isTestnet() ? 6788 : 0;
     // mw-holding score algo. changed; re-calculate the hardware and mw-holding score
-//    public static final int POC_MW_POC_SCORE_CHANGE_HEIGHT = isTestnet() ? 7369 : 0;
-    public static final int POC_MW_POC_SCORE_CHANGE_HEIGHT = isTestnet() ? 7259 : 0;
+    public static final int POC_MW_POC_SCORE_CHANGE_HEIGHT = isTestnet() ? 7288 : 0;
 
     //not opened yet
     public static final int PHASING_BLOCK_HEIGHT = Integer.MAX_VALUE;
