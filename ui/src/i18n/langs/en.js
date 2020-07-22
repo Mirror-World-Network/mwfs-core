5becad1e821384a09ec0652b9e5d2b6b4760e049
Aron-j <xy@ichaoj.com>
2020-07-22 16:46:06 +0800

    rewards tips updated

--- ui/src/i18n/langs/en.js
+++ ui/src/i18n/langs/en.js
@@ -208,7 +208,7 @@ const en = {
         'account_name_not_set_explain':'is the MW amount currently available for use and it will only include the MW earnings from confirmed blocks (after 1 block confirmations).',
         'frozen_balance_nqt': "Freeze Amount",
         'account_mining_balance': "Mined Amount",
-        'account_mining_balance_explain': "Include：mining reward, tx fee",
+        'account_mining_balance_explain': "Include：mining rewards, crowd miner rewards, tx fee",
         'public_key': "Public Key",
     },
     header: {
