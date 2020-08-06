abc3b4857ba2594ac5a2ba6eb9ab134fe3466607
Aron-j <xy@ichaoj.com>
2020-08-07 01:07:06 +0800

    log adjustment; checksum list updated

--- src/main/java/org/conch/chain/CheckSumValidator.java
+++ src/main/java/org/conch/chain/CheckSumValidator.java
@@ -208,6 +208,7 @@ public class CheckSumValidator {
                 +"{\"checksum\":\"7d7eeb8549239f717802ba0af1a5af35b61b622ea612702dc67602ef04d9a50ebbe71db149f90fbe01668b30777f9d25afa4fb3db139b42aba66276be788475b\",\"height\":5316,\"id\":6735474350291239541,\"network\":\"testnet\"},"
                 +"{\"checksum\":\"3a0dbfcef1d5c5b50e979e54038e1291fae84ed95f9504635cccef9c1c6f7e0633b6884ad145ba31c7ed0ebf3f1ed74997f806f2abc3318243d7da1a58984204\",\"height\":5487,\"id\":986005735749606277,\"network\":\"testnet\"},"
                 +"{\"checksum\":\"7f056e9b7b529a7350cb7a41124fe870c1beb6ff87d216fcdb35a9129affed0158a3d75c419c0bafb4a0447cb299051f14a885d35cebf763e16cde2e6a4117d4\",\"height\":5549,\"id\":-6491792554689411068,\"network\":\"testnet\"},"
+                +"{\"checksum\":\"62dd9a649f76ecbe5f7bfac3e3f0237768cbbf3bd87750939dddf3a530f49b06f362cf7040cbda3cfc2b13953e9de2a26a515d255917fa30814d540993c30df8\",\"height\":15483,\"id\":1476485356832204,\"network\":\"testnet\"},"
                 + "]"
         );
 
