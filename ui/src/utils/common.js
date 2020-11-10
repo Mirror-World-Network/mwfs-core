77c33632ddedd99f1c09b5bcee17c6be73dc53e2
Aron-j <xy@ichaoj.com>
2020-11-10 11:06:29 +0800

    feat: reduce query interval of cos ui

--- ui/src/utils/common.js
+++ ui/src/utils/common.js
@@ -7,7 +7,7 @@ export default {
     sharderFoundationHost: 'mw.run',
     sharderFoundationTestHost: 'test.mw.run',
     apiUrl: '',
-    cfg: {defaultInterval: 90000, soonInterval: 30000, slowInterval: 180000},
+    cfg: {defaultInterval: 300000, soonInterval: 60000, slowInterval: 600000},
     epochBeginning: -1,
     newConsole: null,
     isOpenConsole: false,
