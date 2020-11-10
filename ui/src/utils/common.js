6bb5b6e72ff80c34a5c272a55215a43511a4c7c5
Aron-j <xy@ichaoj.com>
2020-11-10 11:49:16 +0800

    feat: reduce query interval of cos ui

--- ui/src/utils/common.js
+++ ui/src/utils/common.js
@@ -21,7 +21,7 @@ export default {
     unit: " MW",
     poolPledgeAmount: 10000000000000, // pledge amount of pool creator
     optHeight: {join: 0, quit: 0, destroy: 0, create: 0},
-    isOpenAirdrop: false, // enabled by the specified user
+    isOpenAirdrop: true, // enabled by the specified user
     isOpenApiProxy: false,
     sendVerifyCode(url, username, fun) {
 
