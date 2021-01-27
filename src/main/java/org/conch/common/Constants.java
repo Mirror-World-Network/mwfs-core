6bb608371e4a3cb80a84ac1ecce94d0da9bc3167
wanbowen001 <wbw157@gmail.com>
2021-01-27 14:54:39 +0800

    feat: adjust sync blocknum = 144

--- src/main/java/org/conch/common/Constants.java
+++ src/main/java/org/conch/common/Constants.java
@@ -258,7 +258,7 @@ public final class Constants {
     public static final String OSS_PREFIX = "https://mwfs.oss-cn-shenzhen.aliyuncs.com/";
 
     //syn
-    public static final int SYNC_BLOCK_NUM = Conch.getIntProperty("sharder.syncblocknum");
+    public static final int SYNC_BLOCK_NUM = Conch.getIntProperty("sharder.sync.blockNum");
     public static final int SYNC_CACHE_BLOCK_NUM = Conch.getIntProperty("sharder.sync.cacheblocknum");
     public static final int SYNC_WORK_BLOCK_NUM = Conch.getIntProperty("sharder.sync.workblocknum");
     public static final Boolean SYNC_BUTTON = Conch.getBooleanProperty("sharder.sync.button", false);
