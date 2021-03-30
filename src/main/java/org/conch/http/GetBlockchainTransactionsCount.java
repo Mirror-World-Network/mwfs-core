b5d81e533dcb628be0271cb488609b969650be64
wanbowen <wbw157@gmail.com>
2021-03-30 15:36:47 +0800

    merge code

--- src/main/java/org/conch/http/GetBlockchainTransactionsCount.java
+++ src/main/java/org/conch/http/GetBlockchainTransactionsCount.java
@@ -34,13 +34,11 @@ public class GetBlockchainTransactionsCount extends APIServlet.APIRequestHandler
             subtype = -1;
         }
 
-        String recipientRS = req.getParameter("recipientRS");
-        String senderRS = req.getParameter("senderRS");
         int count = 0;
         JSONObject response = new JSONObject();
         try{
             if(type != TransactionType.TYPE_POC) {
-                count = Conch.getBlockchain().getTransactionCountByAccount(accountId,type,subtype, true, true, recipientRS, senderRS);
+                count = Conch.getBlockchain().getTransactionCountByAccount(accountId,type,subtype, true, true);
             }
 
             response.put("count",count);
